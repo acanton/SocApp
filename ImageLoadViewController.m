f9a2498b613dc469714867c0a1030b7e4af7df54
Alex Canton <dev_asc@hushmail.com>
2013-11-06 15:10:42 -0600

    removed who’s still up tab view

---
+++ ImageLoadViewController.m
@@ -0,0 +1,38 @@
+//
+//  ImageLoadViewController.m
+//  SocApp
+//
+//  Created by Alexander Canton on 11/5/13.
+//
+//
+
+#import "ImageLoadViewController.h"
+
+@interface ImageLoadViewController ()
+
+@end
+
+@implementation ImageLoadViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
