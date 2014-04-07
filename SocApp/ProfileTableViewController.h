e1bc9241dc95f7a8be9a7797077f2b503e8b50a3
Alex Canton <dev_asc@hushmail.com>
2014-04-07 15:10:33 -0500

    added profile status table view class for profile tab and altered view controller to reflect the assigned class

---
+++ SocApp/ProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.h
+//  SocApp
+//
+//  Created by Alexander Canton on 4/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@end
