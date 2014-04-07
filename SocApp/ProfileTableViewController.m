3c8b50722b2bfb5184dc901a7851c8afde69ec08
Alex Canton <dev_asc@hushmail.com>
2014-04-07 15:10:52 -0500

    added profile status table view class for profile tab and altered view controller to reflect the assigned class

--- SocApp/ProfileTableViewController.m
+++ SocApp/ProfileTableViewController.m
@@ -9,6 +9,16 @@
 #import "ProfileTableViewController.h"
 
 @interface ProfileTableViewController ()
+@property NSMutableArray *LocalPosts;
+@property (strong, nonatomic) IBOutlet UIImageView *ProfilePic;
+@property (strong, nonatomic) IBOutlet UILabel *UserName;
+@property (strong, nonatomic) IBOutlet UILabel *SocPointsView;
+//User Posts
+@property (strong, nonatomic) IBOutlet UITableViewCell *userPost1;
+@property (strong, nonatomic) IBOutlet UITableViewCell *userPost2;
+@property (strong, nonatomic) IBOutlet UITableViewCell *userPost3;
+
+
 
 @end
 
@@ -32,6 +42,9 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.LocalPosts = [[NSMutableArray alloc] init];
+   // [self LoadTestData2];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -55,6 +68,36 @@
     // Return the number of rows in the section.
     return 0;
 }
+
+/*-(void)LoadTestData2
+{
+    
+    //Load test data since we have no data source yet
+    
+    //Load test venue data
+    StatusObject *obj1 = [[StatusObject alloc] init];
+    obj1.statusPost = @"User Post 1";
+    [self.LocalPosts addObject:obj1];
+    self.userPost1.textLabel.text = obj1.statusPost;
+    
+    StatusObject *obj2 = [[StatusObject alloc] init];
+    obj2.VenueName = @"User Post 2";
+    [self.LocalPosts addObject:obj2];
+    self.userPost2.textLabel.text = obj2.statusPost;
+    
+    
+    StatusObject *obj3 = [[StatusObject alloc] init];
+    obj3.statusPost = @"User Post 3";
+    [self.LocalPosts addObject:obj3];
+    self.userPost3.textLabel.text = obj3.statusPost;
+    
+    self.ProfilePic.image = [UIImage imageNamed:@"testimage@2x.png"];
+    self.UserName.text = @"John Doe";
+    self.SocPointsView.text = @"";
+    
+    
+    //Load profile glimpse
+*/
 
 /*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
